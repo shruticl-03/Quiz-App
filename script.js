0973b39df836eb67239ad86104df79971f7d9946
Shruthi L <Shruthi@agpl.net>
2025-04-04 09:57:16 +0530

    Display Answer

--- script.js
+++ script.js
@@ -63,5 +63,12 @@ function strtQuiz() {
 function showQuestion() {
     let currentQuestion = questions[currentQuestionIndex];
     let questionNo = currentQuestionIndex + 1;
-    questionElement.innerHTML = questionNo + " . "+currentQuestion.question;
+    questionElement.innerHTML = questionNo + " . " + currentQuestion.question;
+
+    currentQuestion.answers.forEach(answer => {
+        const button=document.createElement("button");
+        button.innerHTML=answer.text;
+        button.classList.add("btn");
+        answerButton.appendChild(button);
+    });
 }
