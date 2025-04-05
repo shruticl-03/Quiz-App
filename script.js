51362d4cec2cc3019b00854741fa900601b20942
Shruthi L <Shruthi@agpl.net>
2025-04-05 16:28:14 +0530

    Corrected Select Answer field

--- script.js
+++ script.js
@@ -71,6 +71,10 @@ function showQuestion() {
         button.innerHTML = answer.text;
         button.classList.add("btn");
         answerButtons.appendChild(button);
+        if (answer.correct) {
+            button.dataset.correct = answer.correct;
+        }
+        button.addEventListener("click", selectAnswer);
     });
 }
 
@@ -79,6 +83,18 @@ function resetState() {
     while (answerButtons.firstChild) {
         answerButtons.removeChild(answerButtons.firstChild);
     }
+}
+
+function selectAnswer(e) {
+    const selectedBtn = e.target;
+    const isCorrect = selectedBtn.dataset.correct === "true";
+    if (isCorrect) {
+        selectedBtn.classList.add("correct");
+    }
+    else {
+        selectedBtn.classList.add("incorrect");
+    }
+
 }
 
 startQuiz();
