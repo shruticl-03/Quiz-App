61f2127f907f09de381d34c37e028e9f6b3844bb
Shruthi L <Shruthi@agpl.net>
2025-04-05 16:02:39 +0530

    Added reset state

--- script.js
+++ script.js
@@ -47,13 +47,13 @@ const questions = [
 ];
 
 const questionElement = document.getElementById("question");
-const answerButton = document.getElementById("answer-buttons");
+const answerButtons = document.getElementById("answer-buttons");
 const nextButton = document.getElementById("next-btn");
 
 let currentQuestionIndex = 0;
 let score = 0;
 
-function strtQuiz() {
+function startQuiz() {
     currentQuestionIndex = 0;
     score = 0;
     nextButton.innerHTML = "Next";
@@ -61,14 +61,24 @@ function strtQuiz() {
 }
 
 function showQuestion() {
+    resetState();
     let currentQuestion = questions[currentQuestionIndex];
     let questionNo = currentQuestionIndex + 1;
     questionElement.innerHTML = questionNo + " . " + currentQuestion.question;
 
     currentQuestion.answers.forEach(answer => {
-        const button=document.createElement("button");
-        button.innerHTML=answer.text;
+        const button = document.createElement("button");
+        button.innerHTML = answer.text;
         button.classList.add("btn");
-        answerButton.appendChild(button);
+        answerButtons.appendChild(button);
     });
 }
+
+function resetState() {
+    nextButton.style.display = "none";
+    while (answerButtons.firstChild) {
+        answerButtons.removeChild(answerButtons.firstChild);
+    }
+}
+
+startQuiz();
